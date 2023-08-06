32b27dced2ffa6cfbfdb4aae9ddfe4e1dc991e51
ykmxxi <ykmxxi97@gmail.com>
2023-08-06 14:15:25 +0900

    test: split() 메서드 활용 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -10,6 +11,18 @@ public class StringTest {
 	void replace() {
 		String actual = "abc".replace("b", "d");
 		assertThat(actual).isEqualTo("adc");
+	}
+
+	@DisplayName("split() 메서드 활용 학습 테스트")
+	@Test
+	void split() {
+		// given
+		String oneAndTwo = "1,2";
+		String one = "1";
+
+		// when & then
+		assertThat(oneAndTwo.split(",")).contains("1", "2");
+		assertThat(one.split(",")).containsExactly("1");
 	}
 
 }
