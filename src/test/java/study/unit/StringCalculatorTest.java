dec211127e086e94d60410460efc3c649be67701
ykmxxi <ykmxxi97@gmail.com>
2023-08-13 18:14:56 +0900

    feat(StringCalculator): 사칙연산 계산 기능 단위 테스트

--- src/test/java/study/unit/StringCalculatorTest.java
+++ src/test/java/study/unit/StringCalculatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import study.unit.code.StringCalculator;
 
@@ -20,6 +22,14 @@ public class StringCalculatorTest {
 
 		// when & then
 		assertThat(stringCalculator.splitInput(input)).containsExactly(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"2 + 3 * 4 / 2:10", "5 / 5 + 1 * 2:4"}, delimiter = ':')
+	@DisplayName("사칙연산 계산 기능 단위 테스트")
+	void calculate(String input, String expected) {
+		assertThat(stringCalculator.calculate(stringCalculator.splitInput(input)))
+			.isEqualTo(Integer.parseInt(expected));
 	}
 
 }
