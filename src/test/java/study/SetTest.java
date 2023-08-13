688dbac3f4841502bb262825c0ffc577f0d93577
ykmxxi <ykmxxi97@gmail.com>
2023-08-13 17:03:26 +0900

    test: size() 메서드 활용 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("size() 메서드 활용 학습 테스트: Set은 중복된 요소를 허용하지 않는다")
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+}
