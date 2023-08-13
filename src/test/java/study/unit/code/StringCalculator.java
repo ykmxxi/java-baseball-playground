a30c797abdbb6b16930dc523252b9dc49b7ae0e3
ykmxxi <ykmxxi97@gmail.com>
2023-08-13 18:25:30 +0900

    refactor(StringCalculator): 필요없는 필드 제거

--- src/test/java/study/unit/code/StringCalculator.java
+++ src/test/java/study/unit/code/StringCalculator.java
@@ -1,10 +1,6 @@
 package study.unit.code;
 
-import java.util.Scanner;
-
 public class StringCalculator {
-
-	private static final Scanner scanner = new Scanner(System.in);
 
 	public String[] splitInput(String input) {
 		return input.split(" ");
