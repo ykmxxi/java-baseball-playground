1090eeb82d6499802fe34696a93d99990fcaf905
ykmxxi <ykmxxi97@gmail.com>
2023-08-31 17:35:09 +0900

    test: 플레이어 숫자 입력 - 실패 테스트

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
@@ -16,6 +18,17 @@ class InputViewTest {
 
 		// when & then
 		assertThat(inputView.isWrongNumbers(input)).isFalse();
+	}
+
+	@ParameterizedTest()
+	@ValueSource(strings = {"", "  ", "111", "1234", "12"})
+	@DisplayName("플레이어 숫자 입력: 실패 테스트")
+	void readNumbersFailTest(String input) {
+		// given
+		InputView inputView = new InputView();
+
+		// when & then
+		assertThat(inputView.isWrongNumbers(input)).isTrue();
 	}
 
 }
