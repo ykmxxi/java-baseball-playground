14feb9f86c44927a7132bc65055593b0f10c183f
ykmxxi <ykmxxi97@gmail.com>
2023-08-31 16:23:57 +0900

    test: 숫자 야구 번호 생성 - 실패 테스트

--- src/test/java/domain/BaseballTest.java
+++ src/test/java/domain/BaseballTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,17 @@ class BaseballTest {
 
 		// then
 		assertThat(baseball.getNumbers()).containsExactly("1", "2", "3");
+	}
+
+	@Test
+	@DisplayName("숫자 야구 번호 생성: 실패 테스트")
+	void baseballFailTest() {
+		// given
+		String input = "121";
+
+		// when & then
+		assertThatThrownBy(() -> new Baseball(input))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 }
