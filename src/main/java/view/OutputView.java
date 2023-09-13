eda13f05d83fd39132be2985ae10e6e4a003aefb
ykmxxi <ykmxxi97@gmail.com>
2023-09-13 15:56:48 +0900

    feat: 게임 진행 메시지를 출력하는 기능

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,18 @@
+package view;
+
+public class OutputView {
+
+	private static final String INPUT_GUIDE = "숫자를 입력해 주세요 : ";
+	private static final String END_GUIDE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String RETRY_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+	public static void printInputGuide() {
+		System.out.println(INPUT_GUIDE);
+	}
+
+	public static void printEndGuide() {
+		System.out.println(END_GUIDE);
+		System.out.println(RETRY_GUIDE);
+	}
+
+}
