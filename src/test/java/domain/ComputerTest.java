1b8b5dbf15a9f25786188ed17f400852e17d1b91
ykmxxi <ykmxxi97@gmail.com>
2023-10-02 16:43:26 +0900

    test(Computer): 게임이 시작되면 자동으로 숫자를 생성하는 기능 테스트

---
+++ src/test/java/domain/ComputerTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+	@Test
+	void createRandomNumber() {
+		// given
+		Computer computer = new Computer();
+
+		// when
+		List<Integer> result = computer.createRandomNumber();
+
+		// then
+		assertThat(result.size()).isEqualTo(3);
+		assertThat(result.stream()
+						 .allMatch((number) -> number >= 1 && number <= 9)
+		).isTrue();
+	}
+
+}
