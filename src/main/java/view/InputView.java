681a79c2964a3c39f652c94ca5f1c95a43574d7a
ykmxxi <ykmxxi97@gmail.com>
2023-10-04 18:17:03 +0900

    feat(InputView): 플레이어 숫자 입력에 대한 검증 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,44 @@
 package view;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
 	private static final Scanner scanner = new Scanner(System.in);
+	public static final String PLAYER_NUMBER_FORM = "[1-9]{3}";
 
 	public String readPlayerNumber() {
 		return scanner.nextLine();
+	}
+
+	public void validatePlayerNumber(String s) {
+		rejectEmptyOrWhitespaces(s);
+		rejectUnCorrectForm(s);
+		rejectDuplicatedNumber(s);
+	}
+
+	private void rejectEmptyOrWhitespaces(String s) {
+		if (s.isBlank()) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void rejectUnCorrectForm(String s) {
+		if (!s.matches(PLAYER_NUMBER_FORM)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void rejectDuplicatedNumber(String s) {
+		int size = Arrays.stream(s.split(""))
+						 .collect(Collectors.toSet())
+						 .size();
+
+		if (size != 3) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
