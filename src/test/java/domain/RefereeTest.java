c0da114650a9132dc8716975a3f0bf4ed5ac3519
ykmxxi <ykmxxi97@gmail.com>
2023-10-05 14:08:17 +0900

    test(Referee): 컴퓨터와 플레이어의 입력을 비교하는 기능 테스트

---
+++ src/test/java/domain/RefereeTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RefereeTest {
+
+	private Referee referee;
+
+	@BeforeEach
+	void init() {
+		referee = new Referee();
+	}
+
+	@Test
+	void compareOneStrike() {
+		// given
+		Ball computerBall = new Ball(1, 0);
+		Ball playerBall = new Ball(1, 0);
+
+		// when
+		referee.compare(computerBall, playerBall);
+
+		// then
+		assertThat(referee.getStrikeCount()).isEqualTo(1);
+		assertThat(referee.getBallCount()).isEqualTo(0);
+	}
+
+	@Test
+	void compareOneBall() {
+		// given
+		Ball computerBall = new Ball(1, 0);
+		Ball playerBall = new Ball(1, 1);
+
+		// when
+		referee.compare(computerBall, playerBall);
+
+		// then
+		assertThat(referee.getBallCount()).isEqualTo(1);
+		assertThat(referee.getStrikeCount()).isEqualTo(0);
+	}
+
+}
