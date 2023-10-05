ace94c9da7cd7809eefd02fa663eca915b0789c3
ykmxxi <ykmxxi97@gmail.com>
2023-10-05 14:17:11 +0900

    feat(Ball): equals() & hashCode()와 getter 추가

--- src/main/java/domain/Ball.java
+++ src/main/java/domain/Ball.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Ball {
 
 	private final int number;
@@ -10,12 +12,37 @@ public class Ball {
 		this.position = position;
 	}
 
+	public int getNumber() {
+		return number;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
 	public boolean isSameNumber(int number) {
 		return this.number == number;
 	}
 
 	public boolean isSamePosition(int position) {
 		return this.position == position;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null || getClass() != other.getClass()) {
+			return false;
+		}
+		Ball ball = (Ball)other;
+		return number == ball.number && position == ball.position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number, position);
 	}
 
 }
