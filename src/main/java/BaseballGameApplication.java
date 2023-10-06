cb9d438d90c90ccde4c5f227787c4c913d5082a6
ykmxxi <ykmxxi97@gmail.com>
2023-10-06 15:17:36 +0900

    feat(Application): 게임 어플리케이션 구현

---
+++ src/main/java/BaseballGameApplication.java
@@ -0,0 +1,10 @@
+import controller.GameController;
+
+public class BaseballGameApplication {
+
+	public static void main(String[] args) {
+		GameController gameController = new GameController();
+		gameController.run();
+	}
+
+}
