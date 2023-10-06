92cd9e9623de0baa0dc9469dbe2e7adce00ee772
ykmxxi <ykmxxi97@gmail.com>
2023-10-06 15:04:07 +0900

    feat(GameService): 게임의 결과를 판단하는 기능 구현

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,20 +1,41 @@
 package service;
 
+import java.util.List;
+
+import domain.Ball;
 import domain.Computer;
 import domain.Player;
+import domain.Referee;
 
 public class GameService {
 
 	private final Computer computer;
 	private final Player player;
+	private final Referee referee;
 
 	public GameService() {
 		this.computer = new Computer();
 		this.player = new Player();
+		this.referee = new Referee();
 	}
 
 	public void startGame(String playerNumber) {
 		player.changeNumbers(playerNumber);
+	}
+
+	public String judgment() {
+		List<Ball> computerBalls = computer.getNumbers();
+		List<Ball> playerBalls = player.getNumbers();
+		for (Ball computerBall : computerBalls) {
+			compare(playerBalls, computerBall);
+		}
+		return referee.notifyJudgmentResult();
+	}
+
+	private void compare(List<Ball> playerBalls, Ball computerBall) {
+		for (Ball playerBall : playerBalls) {
+			referee.compare(computerBall, playerBall);
+		}
 	}
 
 }
